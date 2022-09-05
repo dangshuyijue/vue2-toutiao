cf2831c2ec249c2f239373085bdbbba931cebb41
itlccx <2629249630@qq.com>
2022-09-05 20:57:41 +0800

    .

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 导入并安装 Vant 组件库
+import Vant from "vant";
+import "vant/lib/index.css";
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false;
 
